62b84a2837056fd713db47ba269d41eb40b11771
Dan-Vy Le <danvyle85@gmail.com>
2019-05-21 19:58:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,6 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = {}
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   return shipping_mainfest
 end
